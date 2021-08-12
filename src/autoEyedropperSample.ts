a4f55aaee3ee0a9ab3b1e0095a9dd0ea0c9f9de5
Lai, Yu-Hsuan <raincolee@gmail.com>
2021-08-13 00:26:21 +0800

    Comment

--- src/autoEyedropperSample.ts
+++ src/autoEyedropperSample.ts
@@ -7,10 +7,7 @@ import { BlendMode } from './types/photoshop/Common'
 
 export function setupAutoEyedropperSample(): void {
     photoshop.action.addNotificationListener(['select', 'set', 'make', 'delete'], async (event: string, descriptor: ActionDescriptor) => {
-        // All these events can change the status of active layer:
-        // 'select': select a layer;
-        // 'set': set active layer's blend mode
-        // 'make': create a new layer
+        // All these events can target a layer
         if (isTargetingLayer(descriptor)) {
             // Store current tool so we can switch back...
             let currentTool = await getTool();
